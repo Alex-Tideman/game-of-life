e1bad6968660c8abddfda02e477183b483232d7f
Alex Tideman <alex.tideman@gmail.com>
2019-03-07 21:58:12 -0700

    Update logic to check all 8 neighbors

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,43 @@
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import { useInterval } from '../hooks'
 
 import './App.css'
 
 const initialSeed = []
-for (let i = 1; i <= 20; i++) {
-  initialSeed.push(Math.floor((Math.random() * 100) + 1))
+for (let i = 1; i <= 300; i++) {
+  initialSeed.push(Math.floor((Math.random() * 1000) + 1))
+}
+
+const initialCells = []
+for (let i = 1; i <= 1000; i++) {
+  initialCells.push(<Cell i={i} alive={initialSeed.includes(i) ? 1 : 0} />)  
 }
 
 function App() {
-  const [cells, setCells] = useState(initialSeed)
+  const [cells, setCells] = useState(initialCells)
   const [count, setCount] = useState(0)
-  const [delay, setDelay] = useState(1000)
+  const delay = 1000
 
   useInterval(() => {
-    const nextCells = []
-    for (let i = 1; i <= 100; i++) {
-      if (count === 0) {
-        nextCells.push(<Cell i={i} alive={initialSeed.includes(i)} />)  
-      } else {
-        const alive = i % 3 === 0
-        nextCells.push(<Cell i={i} alive={initialSeed.includes(i + count)} />)
-      }
+    if (count > 0) {
+      const nextCells = cells.map((cell, i) => {
+        const alive = isAlive(cells, cell, i)
+        return <Cell i={i} alive={alive ? 1 : 0} />
+      })
+      setCells(nextCells)
     }
-    setCells(nextCells)
     setCount(count + 1)
-    window.document.title = count + 1
+    window.document.title = count + 1  
   }, delay)
 
   return (
     <div className="AppContainer">
       <div className="App">
+      {!count && <h1>Life Begins...</h1>}
+      {count && 
         <div className="Life">
-          {count > 0 ? cells : 'Life Begins...'}
-        </div>
+          {cells}
+        </div>}
       </div>
     </div>
   )
@@ -46,4 +50,26 @@ function Cell({ i, alive }) {
     <div key={i} className={`Cell ${alive ? 'Alive' : 'Dead'}`}>
     </div>
   )
+}
+
+function isAlive(cells, cell, i) {
+  const leftCell = cells[i - 1] ? cells[i - 1].props.alive : 0
+  const rightCell = cells[i + 1] ? cells[i + 1].props.alive : 0
+  const aboveCell = cells[i - 10] ? cells[i - 10].props.alive : 0
+  const belowCell = cells[i + 10] ? cells[i + 10].props.alive : 0
+  const leftAboveCell = cells[i - 11] ? cells[i - 11].props.alive : 0
+  const rightAboveCell = cells[i - 9] ? cells[i - 9].props.alive : 0
+  const leftBelowCell = cells[i + 9] ? cells[i + 9].props.alive : 0
+  const rightBelowCell = cells[i + 11] ? cells[i + 11].props.alive : 0
+
+  const aliveNeighbors = leftCell + rightCell + aboveCell + belowCell + leftAboveCell + rightAboveCell + leftBelowCell + rightBelowCell
+
+  let alive = false
+  if ((aliveNeighbors === 2 || aliveNeighbors === 3) && cell.props.alive) {
+    alive = true
+  } else if (aliveNeighbors === 3 && !cell.props.alive) {
+    alive = true
+  }
+
+  return alive
 }
